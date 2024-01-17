795cf2b91f096958ed4dc75e3aa899e0f16c6d28
Jeremy Coando <killerpit02@yahoo.com>
2024-01-17 10:56:06 -0700

    adding flake8 .flak8

--- devops_app/devops_app.py
+++ devops_app/devops_app.py
@@ -2,10 +2,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-
 def dev_ops():
     return 'DevOps App - Terraform, Kubernetes, GithubActions'
 
 if __name__ == '__main__':
-    
     app.run(host='0.0.0.0', port=80)
